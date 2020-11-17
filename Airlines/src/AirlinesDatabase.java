004bd6eabbdd0939dc6670c9a6691b067f4007a8
Vladimir Vozniak <vozniak.vladimir90@gmail.com>
2020-11-17 14:19:34 +0300

    Add some changes to classes Main and AirlinesDatabase

--- Airlines/src/AirlinesDatabase.java
+++ Airlines/src/AirlinesDatabase.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class AirlinesDatabase {
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private static Airline[] airlines;
 
     public AirlinesDatabase(Airline... newAirlines) {
@@ -23,6 +28,58 @@ public class AirlinesDatabase {
         for (; index2 < newAirlines.length; index2++) {
             airlines[index] = newAirlines[index2];
             index++;
+        }
+    }
+
+    public void chooseOption() throws IOException {
+        boolean stop = false;
+        int option = 0;
+        do{
+            System.out.println("Please choose one of the suggested options and enter its number.");
+            System.out.println("1. Display a list of flights for a given destination.");
+            System.out.println("2. Display a list of flights for a given day of the week.");
+            System.out.println("3. Display a list of flights for a given day of the week that have " +
+                    "a longer departure time than the specified one.");
+            try {
+                option = Integer.parseInt(reader.readLine());
+            } catch (Exception e) {
+                System.out.println("Invalid data.");
+            }
+            if(option == 1 || option == 2 || option == 3) stop = true;
+            else System.out.println("Please try again.");
+        } while (!stop);
+        getFlights(option);
+    }
+
+    private void getFlights(int option) throws IOException {
+        switch (option) {
+            case 1 -> {
+                System.out.println("Please enter destination:");
+                String destination = reader.readLine();
+                for (int i = 0; i < this.getAirlines().length; i++) {
+                    if(this.getAirlines()[i].getDestination().equalsIgnoreCase(destination))
+                        System.out.println(this.getAirlines()[i]);
+                }
+            }
+            case 2 -> {
+                System.out.println("Please enter day of the week:");
+                String day = reader.readLine();
+                for (int i = 0; i < this.getAirlines().length; i++) {
+                    if(this.getAirlines()[i].getDayOfWeek().equalsIgnoreCase(day))
+                        System.out.println(this.getAirlines()[i]);
+                }
+            }
+            case 3 -> {
+                System.out.println("Please enter day of the week:");
+                String day = reader.readLine();
+                System.out.println("Please enter time(hh.mm):");
+                double time = Double.parseDouble(reader.readLine());
+                for (int i = 0; i < this.getAirlines().length; i++) {
+                    double time1 = Double.parseDouble(this.getAirlines()[i].getTimeOfDeparture());
+                    if(this.getAirlines()[i].getDayOfWeek().equalsIgnoreCase(day) && time1 > time)
+                        System.out.println(this.getAirlines()[i]);
+                }
+            }
         }
     }
 }
--- Airlines/src/Main.java
+++ Airlines/src/Main.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
-    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         Airline airline1 = new Airline("Moscow", 12345, "Boeing 767",
                 "10.30", "Monday");
@@ -16,58 +13,6 @@ public class Main {
         Airline airline5 = new Airline("Munich", 65421, "Boeing 767",
                 "11.40", "Friday");
         AirlinesDatabase database = new AirlinesDatabase(airline1, airline2, airline3, airline4, airline5);
-        chooseOption(database);
-    }
-
-    private static void chooseOption(AirlinesDatabase database) throws IOException {
-        boolean stop = false;
-        int option = 0;
-        do{
-            System.out.println("Please choose one of the suggested options and enter its number.");
-            System.out.println("1. Display a list of flights for a given destination.");
-            System.out.println("2. Display a list of flights for a given day of the week.");
-            System.out.println("3. Display a list of flights for a given day of the week that have " +
-                    "a longer departure time than the specified one.");
-            try {
-                option = Integer.parseInt(reader.readLine());
-            } catch (Exception e) {
-                System.out.println("Invalid data.");
-            }
-            if(option == 1 || option == 2 || option == 3) stop = true;
-            else System.out.println("Please try again.");
-        } while (!stop);
-        getFlights(option, database);
-    }
-
-    private static void getFlights(int option, AirlinesDatabase database) throws IOException {
-        switch (option) {
-            case 1 -> {
-                System.out.println("Please enter destination:");
-                String destination = reader.readLine();
-                for (int i = 0; i < database.getAirlines().length; i++) {
-                    if(database.getAirlines()[i].getDestination().equalsIgnoreCase(destination))
-                        System.out.println(database.getAirlines()[i]);
-                }
-            }
-            case 2 -> {
-                System.out.println("Please enter day of the week:");
-                String day = reader.readLine();
-                for (int i = 0; i < database.getAirlines().length; i++) {
-                    if(database.getAirlines()[i].getDayOfWeek().equalsIgnoreCase(day))
-                        System.out.println(database.getAirlines()[i]);
-                }
-            }
-            case 3 -> {
-                System.out.println("Please enter day of the week:");
-                String day = reader.readLine();
-                System.out.println("Please enter time(hh.mm):");
-                double time = Double.parseDouble(reader.readLine());
-                for (int i = 0; i < database.getAirlines().length; i++) {
-                    double time1 = Double.parseDouble(database.getAirlines()[i].getTimeOfDeparture());
-                    if(database.getAirlines()[i].getDayOfWeek().equalsIgnoreCase(day) && time1 > time)
-                        System.out.println(database.getAirlines()[i]);
-                }
-            }
-        }
+        database.chooseOption();
     }
 }
